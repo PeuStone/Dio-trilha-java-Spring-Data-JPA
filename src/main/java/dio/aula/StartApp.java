74fecddcf0b8bf4d64a089f5874179b164db661d
PeuStone <pedrohmotaaa@gmail.com>
2024-10-25 11:25:06 -0300

    Realizando pesquisas no BD utilizando Query Method e Query Override.

--- src/main/java/dio/aula/StartApp.java
+++ src/main/java/dio/aula/StartApp.java
@@ -6,12 +6,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class StartApp implements CommandLineRunner {
     @Autowired
     private UserRepository repository;
     @Override
     public void run(String... args) throws Exception {
+//        List<User> users = repository.filtrarPorNome("Joaquim");
+        List<User> users = repository.findByNameContaining("Joaquim");
+        for (User u: users) {
+            System.out.println(u);
+        }
+    }
+
+    private void insertUser() {
         User user = new User();
         user.setName("Joaquim");
         user.setUsername("Jojo");
